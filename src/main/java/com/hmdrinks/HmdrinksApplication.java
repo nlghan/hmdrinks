c9859be2e8a5e1e3197e2fd33d2a492e54b16d0c
nlghan <giahan05092003@gmail.com>
2024-10-06 00:25:45 +0700

    hello

--- src/main/java/com/hmdrinks/HmdrinksApplication.java
+++ src/main/java/com/hmdrinks/HmdrinksApplication.java
@@ -2,8 +2,10 @@ package com.hmdrinks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.hmdrinks")
+@EnableJpaRepositories(basePackages = "com.hmdrinks.Repository")
 public class HmdrinksApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hmdrinks/Repository/UserRepository.java
+++ src/main/java/com/hmdrinks/Repository/UserRepository.java
@@ -4,12 +4,13 @@ import com.hmdrinks.Entity.OTP;
 import com.hmdrinks.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
+@Repository
+ public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUserId(int userId);
 
     User findByEmail(String email);
