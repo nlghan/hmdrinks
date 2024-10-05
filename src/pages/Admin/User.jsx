169cd0f370d4b4ff1aa8c108a26810e2fcf3e98e
My281203 <nguyenthuydiemmy2003tv@gmail.com>
2024-10-05 18:51:18 +0700

    fix lặp css

--- src/pages/Admin/User.jsx
+++ src/pages/Admin/User.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './User.css';
+import { assets } from '../../assets/assets';
 
-const Dashboard = () => {
+const User = () => {
     const [switches, setSwitches] = useState([true, false]); // Mảng để lưu trạng thái cho các switch
 
     const handleSwitchChange = (index) => {
@@ -10,12 +11,63 @@ const Dashboard = () => {
         setSwitches(newSwitches);
     };
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // State to toggle menu visibility
+
+    // Function to toggle the menu
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div className="dashboard">
-            <div className="dashboard-row">
-                <div className="main-section">
-                    <div className="orders-box">
-                        <div className="header">
+        <div className="user-table">
+            {isMenuOpen && <div className="user-dim-background" onClick={toggleMenu}></div>}
+
+            <div className='user-menu-flex'>
+                {isMenuOpen && (
+                    <div className="user-side-menu">
+
+                        <ul className="user-menu-items">
+                            <img src={assets.logo} alt='' className="user-logo-menu" />
+                            <div className='user-menu-and-user'>
+                                <i className='ti-user' />
+                                <li>Tài khoản</li>
+                            </div>
+
+                            <div className='user-menu-and-user'>
+                                <i className='ti-package' />
+                                <li>Sản phẩm</li>
+                            </div>
+                            <div className='user-menu-and-user'>
+                                <i className='ti-pencil-alt' />
+                                <li>Đơn hàng</li>
+                            </div>
+                            <div className='user-menu-and-user'>
+                                <i className='ti-signal' />
+                                <li>Tiếp thị</li>
+                            </div><div className='user-menu-and-user'>
+                                <i className='ti-share-alt' />
+                                <li>Phản hồi</li>
+                            </div>
+                            <div className='user-menu-and-user'>
+                                <i className='ti-image' />
+                                <li>Analytics</li>
+                            </div>
+
+                            <div className='user-menu-and-user'>
+                                <i className='ti-back-left' />
+                                <li>Logout</li>
+                            </div>
+
+
+                        </ul>
+                    </div>
+                )}
+            </div>
+            <div className={`user-table-row ${isMenuOpen ? 'user-dimmed' : ''}`}>
+                <div className="user-main-section">
+
+                    <div className="user-box">
+                        <div className="header-user-box">
                             <h2>Users</h2>
                             <button className="add-user-btn">Thêm người dùng +</button>
                         </div>
@@ -46,8 +98,8 @@ const Dashboard = () => {
                                             </label>
                                         </td>
                                         <td>
-                                            <button className="update-btn1">Cập nhật</button>
-                                            <button className="update-btn2">Xóa</button>                                           
+                                            <button className="user-update-btn1">Cập nhật</button>
+                                            <button className="user-update-btn2">Xóa</button>
                                         </td>
                                     </tr>
                                 ))}
@@ -55,52 +107,57 @@ const Dashboard = () => {
                         </table>
                     </div>
 
-                    <div className="stats-section">
-                        <div className="stat-box1">
-                            <div className="percentage-circle">
-                                <div className="inner-circle"></div>
+                    <div className="user-stats-section">
+                        <div className="user-stat-box">
+                            <i className="ti-menu menu-toggle" onClick={toggleMenu}>
+                            </i>
+                        </div>
+
+                        <div className="user-stat-box1">
+                            <div className="user-percentage-circle">
+                                <div className="user-inner-circle"></div>
                                 <span>70%</span>
                             </div>
-                            <div className="icon"></div>
-                            <div className="text1">
+                            <div className="user-stat-boxicon"></div>
+                            <div className="user-stat-boxtext1">
                                 <h3>Admin</h3>
                             </div>
-                            <div className="count">
+                            <div className="user-stat-boxcount">
                                 <h4></h4>
                             </div>
-                            <div className="details">
+                            <div className="user-stat-boxdetails">
                                 <h5>Last 24 Hours</h5>
                             </div>
                         </div>
-                        <div className="stat-box2">
-                            <div className="percentage-circle2">
-                                <div className="inner-circle2"></div>
+                        <div className="user-stat-box2">
+                            <div className="user-percentage-circle2">
+                                <div className="user-inner-circle2"></div>
                                 <span>80%</span>
                             </div>
-                            <div className="icon"></div>
-                            <div className="text1">
+                            <div className="user-stat-boxicon"></div>
+                            <div className="user-stat-boxtext1">
                                 <h3>Customer</h3>
                             </div>
-                            <div className="count">
+                            <div className="user-stat-boxcount">
                                 <h4></h4>
                             </div>
-                            <div className="details">
+                            <div className="user-stat-boxdetails">
                                 <h5>Last 24 Hours</h5>
                             </div>
                         </div>
-                        <div className="stat-box3">
-                            <div className="percentage-circle3">
-                                <div className="inner-circle3"></div>
+                        <div className="user-stat-box3">
+                            <div className="user-percentage-circle3">
+                                <div className="user-inner-circle3"></div>
                                 <span>60%</span>
                             </div>
-                            <div className="icon"></div>
-                            <div className="text1">
+                            <div className="user-stat-boxicon"></div>
+                            <div className="user-stat-boxtext1">
                                 <h3>Shipper</h3>
                             </div>
-                            <div className="count">
+                            <div className="user-stat-boxcount">
                                 <h4></h4>
                             </div>
-                            <div className="details">
+                            <div className="user-stat-boxdetails">
                                 <h5>Last 24 Hours</h5>
                             </div>
                         </div>
@@ -112,4 +169,4 @@ const Dashboard = () => {
     );
 };
 
-export default Dashboard;
+export default User;
