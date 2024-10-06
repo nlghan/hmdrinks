32bccfbb09280c52625fae7328a4a49564b5201b
giahann <giahan05092003@gmail.com>
2024-10-06 22:34:01 +0700

    Sửa lại giao diện user của admin dùng component Header

--- src/pages/Admin/User.jsx
+++ src/pages/Admin/User.jsx
@@ -128,9 +128,6 @@ const User = () => {
             <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} title="Tài khoản" />
             <div className={`user-table-row ${isMenuOpen ? 'user-dimmed' : ''}`}>
                 <div className="user-main-section">
-                    <div className='user-flex'>
-                        <h1>Tài khoản</h1>
-                    </div>
                     <div className="user-box">
                         <div className="header-user-box">
                             <h2>Danh Sách Người Dùng</h2>
@@ -164,9 +161,13 @@ const User = () => {
                                             </label>
                                         </td>
                                         <td>
-                                            <button className="user-update-btn1">Cập nhật</button>
-                                            <button className="user-update-btn2">Xóa</button>
+                                            <div className="button-container">
+                                                <button className="user-update-btn3">Chi tiết</button>
+                                                <button className="user-update-btn1">Cập nhật</button>
+                                                <button className="user-update-btn2">Xóa</button>
+                                            </div>
                                         </td>
+
                                     </tr>
                                 ))}
                             </tbody>
@@ -174,10 +175,6 @@ const User = () => {
                     </div>
                 </div>
                 <div className="user-stats-section">
-                    <div className="user-stat-box">
-                        <i className="ti-menu menu-toggle" onClick={toggleMenu}>
-                        </i>
-                    </div>
 
                     <div className="user-stat-box1">
                         <div className="user-percentage-circle">
