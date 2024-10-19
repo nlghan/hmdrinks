509e6d7f36c847e2ac9f1c6a8711160780f5b4d4
nlghan <giahan05092003@gmail.com>
2024-10-19 22:47:44 +0700

    rate limit

--- src/main/java/com/hmdrinks/HmdrinksApplication.java
+++ src/main/java/com/hmdrinks/HmdrinksApplication.java
@@ -1,9 +1,12 @@
 package com.hmdrinks;
 
+import com.hmdrinks.Config.RateLimitingFilter;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "HMDrinks Server URL")})
 @SpringBootApplication(scanBasePackages = "com.hmdrinks")
@@ -11,4 +14,5 @@ public class HmdrinksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HmdrinksApplication.class, args);
 	}
+
 }
