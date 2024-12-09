0de3f3cc13d0dab045d94cde0235dbf20aee3931
My281203 <nguyenthuydiemmy2003tv@gmail.com>
2024-12-09 21:51:44 +0700

    update

--- src/main/java/com/hmdrinks/Service/ZaloPayService.java
+++ src/main/java/com/hmdrinks/Service/ZaloPayService.java
@@ -189,7 +189,7 @@ public class ZaloPayService {
         order.put("amount", total);
         order.put("description", "HMDrinks - Payment for the order #" + transId);
         order.put("bank_code", "");
-        order.put("callback_url", "https://e6e2-42-116-39-1.ngrok-free.app/payment-online-status/");
+        order.put("callback_url", "https://cff4-171-232-181-39.ngrok-free.app/payment-online-status/");
         order.put("embed_data", "{\"redirecturl\":\"http://localhost:5173/payment-online-status\"}");
         order.put("item", "[]");
 
